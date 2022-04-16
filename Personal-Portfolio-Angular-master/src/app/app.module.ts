0347b90cdf5988565cf45e49ed612f540bcf8c58
Tubishat98 <mohammad.tubishat@pwc.com>
2022-04-16 22:10:48 +0300

    Protfolio
    Protfolio

---
+++ Personal-Portfolio-Angular-master/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule , CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { RouterModule, Routes, ExtraOptions } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileModule } from './profile/profile.module';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProfileComponent
+  }
+];
+
+const config: ExtraOptions = {
+  useHash: true,
+};
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes, config),
+    ProfileModule,
+    HttpClientModule
+  ],
+  providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Personal-Portfolio-Angular-master/src/app/profile/profile.module.ts
@@ -0,0 +1,46 @@
+import { NgModule , CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SnotifyModule, SnotifyService, ToastDefaults } from 'ng-snotify';
+import { ProfileComponent } from './profile.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { IntroComponent } from './intro/intro.component';
+import { AboutComponent } from './about/about.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    SnotifyModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxSpinnerModule,
+    
+  ],
+  declarations: [
+    ProfileComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    IntroComponent,
+    ContactComponent,
+    EducationComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    
+  ],
+  providers: [
+    { provide: 'SnotifyToastConfig', useValue: ToastDefaults},
+    SnotifyService,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+})
+export class ProfileModule { }
